ebdfede03c1a4a402a6e3910d28aae7fd502eb00
hupan <hupan@itheima.com>
2018-09-24 01:01:14 +0800

    日志信息,第一用idea里面的git 2018-9-24 01:01:00

---
+++ src/com/itheima/git/firstclass.java
@@ -0,0 +1,12 @@
+package com.itheima.git;
+
+public class firstclass {
+    public static void main(String[] args) {
+        System.out.println("你好啊");
+        System.out.println("你好啊aaa");
+        System.out.println("你好啊bbb");
+        System.out.println("你好啊ccc");
+        System.out.println("你好啊ddd");
+    }
+
+}
